bc3ab9aaf4b7ff05499f9bfa08b24795cf6b946d
Simiyu Daniel <simiyuwdan@gmail.com>
2025-01-17 15:33:42 +0300

    Check null values

--- jgp-database-web/src/app/pages/dashboard/info-cards/info-cards.component.ts
+++ jgp-database-web/src/app/pages/dashboard/info-cards/info-cards.component.ts
@@ -432,10 +432,7 @@ export class InfoCardsComponent implements OnInit, AfterViewChecked, OnChanges, 
 
   valueFormatting = (value: number) => {
     // Custom value formatting: Convert to a string with thousands separator
-    if(!value){
-      return '0'
-    }
-    return value.toLocaleString(); // Outputs as "8,940,000"
+    return value ? value.toLocaleString() : '0'; // Outputs as "8,940,000"
   };
 
 
