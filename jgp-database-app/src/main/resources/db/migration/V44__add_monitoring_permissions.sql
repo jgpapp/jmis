a07e6d38cb0cf6938e8d52669ac2c12f4c0776a4
Daniel Simiyu <simiyuwdan@gmail.com>
2025-06-28 13:07:42 +0300

    Add permission for each template upload

---
+++ jgp-database-app/src/main/resources/db/migration/V44__add_monitoring_permissions.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO permission (entity_name, action_name, code)VALUES('MONITORING_OUTCOME', 'UPLOAD', 'MONITORING_OUTCOME_UPLOAD');
+INSERT INTO permission (entity_name, action_name, code)VALUES('MONITORING_OUTCOME', 'READ', 'MONITORING_OUTCOME_READ');
+INSERT INTO permission (entity_name, action_name, code)VALUES('MONITORING_OUTCOME', 'APPROVE', 'MONITORING_OUTCOME_APPROVE');
+INSERT INTO permission (entity_name, action_name, code)VALUES('MENTOR_SHIP', 'UPLOAD', 'MENTOR_SHIP_UPLOAD');
+INSERT INTO permission (entity_name, action_name, code)VALUES('NOTIFICATION', 'GET_MENTORSHIP', 'NOTIFICATION_GET_MENTORSHIP');
+INSERT INTO permission (entity_name, action_name, code)VALUES('NOTIFICATION', 'GET_LOAN', 'NOTIFICATION_GET_LOAN');
+INSERT INTO permission (entity_name, action_name, code)VALUES('NOTIFICATION', 'GET_TA', 'NOTIFICATION_GET_TA');
+INSERT INTO permission (entity_name, action_name, code)VALUES('NOTIFICATION', 'GET_MONITORING', 'NOTIFICATION_GET_MONITORING');
+
