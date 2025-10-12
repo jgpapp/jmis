9740ea2fd007a4732f9767aa61adf89c35a89a59
Daniel Simiyu <simiyuwdan@gmail.com>
2025-10-12 10:16:13 +0300

    Add PG_PASSWORD for database restoration
    Added password environment variable for PostgreSQL restore.

--- db_restoration.sh
+++ db_restoration.sh
@@ -2,11 +2,15 @@
 PG_USER="root"       # The user that owns the database and will perform the restore
 PG_PORT="5433"       # The port of the PostgreSQL instance you're restoring to
 DB_NAME="jgp_restored"       # The name of the database you're restoring into
+PG_PASSWORD='db_password'      # Uncomment and set if not using .pgpass or trust authentication
 
 # Path to your backup file (e.g., from local backups or copied to /tmp)
 BACKUP_FILE_PATH="/tmp/jgp_app_db_20250726_131815.sqlc.gz"
 
 echo "Restoring database '$DB_NAME' from '$BACKUP_FILE_PATH'..."
+
+# Set PGPASSWORD for the psql command
+export PGPASSWORD="$PG_PASSWORD"
 
 # Restore command:
 # gunzip -c : decompresses the file and pipes its content to stdout
