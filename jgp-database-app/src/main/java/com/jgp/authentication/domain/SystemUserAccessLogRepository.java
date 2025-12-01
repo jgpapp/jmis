b86cb61f535b9bc0ca6b899508ec2fab13513571
Daniel Simiyu <simiyuwdan@gmail.com>
2025-12-01 11:50:35 +0300

    Dashboard

--- jgp-database-app/src/main/java/com/jgp/authentication/domain/SystemUserAccessLogRepository.java
+++ jgp-database-app/src/main/java/com/jgp/authentication/domain/SystemUserAccessLogRepository.java
@@ -2,18 +2,27 @@ package com.jgp.authentication.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.Optional;
+import java.time.LocalTime;
 
 @Repository
 public interface SystemUserAccessLogRepository extends JpaRepository<SystemUserAccessLog, Long>, JpaSpecificationExecutor<SystemUserAccessLog>,
         QuerydslPredicateExecutor<SystemUserAccessLog> {
 
-    Optional<SystemUserAccessLog> findByUsernameAndLoginDateAndLoginHourAndIsDeletedFalse(@NonNull String username, @NonNull LocalDate loginDate, @NonNull Integer loginHour);
+    @Modifying
+    @Transactional
+    @Query(value = """
+            INSERT INTO system_user_access_logs (username, ip_address, login_time, login_date, login_hour) 
+            VALUES (?1, ?2, ?3, ?4, ?5) 
+            ON CONFLICT (username, login_date, login_hour) DO UPDATE set last_modified = current_timestamp 
+            """, nativeQuery = true)
+    void saveUserLogin(String username, String ipAddress, LocalTime loginTime, LocalDate loginDate, Integer loginHour);
 
 
 }
