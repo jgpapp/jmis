56b585eef7e1967bed21df5f122d33fd5dca00d7
Daniel Simiyu <simiyuwdan@gmail.com>
2026-01-16 11:04:29 +0300

    Docker improvements

--- jgp-database-web/public/env.template.js
+++ jgp-database-web/public/env.template.js
@@ -3,8 +3,8 @@
     window.__env = window.__env || {};
   
     // Environment variables will be populated here by the entrypoint script
-    window.__env.baseApiUrl = '${JGP_APP_BASE_URL}';
-    window.__env.baseWebSocketUrl = '${JGP_APP_BASE_WEBSOCKET_URL}';
+    window.__env.baseApiUrl = '/api';
+    window.__env.baseWebSocketUrl = '/ws';
   
     // Add more variables as needed
     // window.__env.anotherVar = '${ANOTHER_VAR}';
